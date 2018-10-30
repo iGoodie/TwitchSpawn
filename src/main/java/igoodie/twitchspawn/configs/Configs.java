afe5873a72f7f0a8aae14652e790a2a7556a27b5
Anılcan <xgen_blue@windowslive.com>
2018-10-30 03:07:31 +0300

    Improve config validation

--- src/main/java/igoodie/twitchspawn/configs/Configs.java
+++ src/main/java/igoodie/twitchspawn/configs/Configs.java
@@ -1,6 +1,8 @@
 package igoodie.twitchspawn.configs;
 
+import igoodie.twitchspawn.TwitchSpawn;
 import igoodie.twitchspawn.utils.FileUtils;
+import net.minecraft.util.StringUtils;
 
 import java.io.File;
 
@@ -13,28 +15,69 @@ public class Configs {
 	public static JsonObject json;
 
 	public static String CONFIG_DIR;
-	
+
 	public static void init(File file) {
 		//Save direction of TwitchSpawn configs
 		CONFIG_DIR = file.getParent() + File.separatorChar + "TwitchSpawn";
-		
-		//Pre-validate
-		ConfigsValidator.preValidate();
-		
-		//Load from configs
+
 		load();
 		
-		//Validate
-		ConfigsValidator.validate();
+		if(validate())
+			save();
+		
+		// TODO: Remove invalid keys
+	}
+
+	private static boolean validate() {
+		boolean changesMade = false;
+		
+		// Create config dir if not present
+		if(!FileUtils.fileExists(CONFIG_DIR)) {
+			FileUtils.createDir(CONFIG_DIR);
+			changesMade = true;
+		}
+		
+		// Add tokens
+		changesMade |= addValue("access_token", "!Your Streamlabs API Access Token here!");
+		changesMade |= addValue("socket_api_token", "!Your Streamlabs Socket API Token here!");
+		
+		// Add nicks
+		changesMade |= addValue("streamer_mc_nick", "!Your Minecraft nick here!");
+		changesMade |= addValue("streamer_twitch_nick", "!Your Twitch channel name here!");
+		
+		// Add example selectors
+		String selectors = "{\"bit_rewards\":[{\"minimum_bit\":0,\"items\":[\"minecraft:stick\",\"minecraft:apple\"]},{\"minimum_bit\":100,\"items\":[\"minecraft:diamond_block\"]}],\"donation_rewards\":[{\"minimum_amount\":0,\"items\":[\"minecraft:stick\",\"minecraft:apple\"]}],\"sub_rewards\":[{\"minimum_months\":0,\"items\":[\"minecraft:stick\",\"minecraft:apple\"]},{\"minimum_months\":10,\"items\":[\"minecraft:diamond_block\"]}]}";
+		changesMade |= addValue("rewards", new JsonParser().parse(selectors).getAsJsonObject());
+		
+		return changesMade;
 	}
 	
+	private static boolean addValue(String key, JsonElement value) {
+		if(json.has(key)) return false; // Changes not made
+		
+		json.add(key, value);
+		return true; // Changes made
+	}
+	
+	private static boolean addValue(String key, String value) {
+		if(json.has(key)) return false; // Changes not made
+		
+		json.addProperty(key, value);
+		return true; // Changes made
+	}
+
 	public static void save() {
 		FileUtils.writeString(beautifyJson(json), CONFIG_DIR+File.separatorChar+"config.json");
 	}
-	
+
 	public static void load() {
 		String jsonStr = FileUtils.readString(CONFIG_DIR+File.separatorChar+"config.json");
-		jsonStr = jsonStr==null||jsonStr.isEmpty() ? "{}" : jsonStr;
+		
+		if(StringUtils.isNullOrEmpty(jsonStr)) {
+			jsonStr = "{}";
+			TwitchSpawn.LOGGER.warn("TwitchSpawn config loaded as {}. Something suspicious happens hier..");
+		}
+		
 		json = new JsonParser().parse(jsonStr).getAsJsonObject();
 	}
 
