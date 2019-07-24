f80ae6879e5944e625b7e99fe77e4455886fc52a
Anılcan <igoodie@programmer.net>
2019-07-25 02:27:02 +0300

    fix(tsl): forced case sensitivity on rule set tsl files

--- src/main/java/net/programmer/igoodie/twitchspawn/TwitchSpawn.java
+++ src/main/java/net/programmer/igoodie/twitchspawn/TwitchSpawn.java
@@ -18,8 +18,11 @@ import net.programmer.igoodie.twitchspawn.command.TwitchSpawnCommand;
 import net.programmer.igoodie.twitchspawn.configuration.ConfigManager;
 import net.programmer.igoodie.twitchspawn.tracer.StreamlabsSocketClient;
 import net.programmer.igoodie.twitchspawn.tslanguage.parser.TSLParser;
+import net.programmer.igoodie.twitchspawn.tslanguage.parser.TSLSyntaxError;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.List;
 
 @Mod(TwitchSpawn.MOD_ID)
 public class TwitchSpawn {
