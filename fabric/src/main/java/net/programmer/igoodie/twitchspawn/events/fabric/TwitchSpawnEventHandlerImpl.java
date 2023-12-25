776fa7488f57764334b70bbdad0438d32ef47294
BONNe <bonne@bonne.id.lv>
2023-12-25 02:09:05 +0200

    Replace fabric screen implementation with new implementation in architectury api.

--- fabric/src/main/java/net/programmer/igoodie/twitchspawn/events/fabric/TwitchSpawnEventHandlerImpl.java
+++ fabric/src/main/java/net/programmer/igoodie/twitchspawn/events/fabric/TwitchSpawnEventHandlerImpl.java
@@ -7,18 +7,6 @@
 package net.programmer.igoodie.twitchspawn.events.fabric;
 
 
-import dev.architectury.event.events.common.LifecycleEvent;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.screens.ErrorScreen;
-import net.minecraft.client.gui.screens.Overlay;
-import net.programmer.igoodie.twitchspawn.TwitchSpawn;
-import net.programmer.igoodie.twitchspawn.events.TwitchSpawnCommonEvent;
-
-
 /**
  * This class manages the registration of events.
  */
@@ -29,21 +17,6 @@ public class TwitchSpawnEventHandlerImpl
      */
     public static void registerClient()
     {
-        // Register error for incorrect configs.
-        TwitchSpawnCommonEvent.SETUP_EVENT.register(exception -> {
-            ClientLifecycleEvents.CLIENT_STARTED.register(client ->
-                Minecraft.getInstance().setOverlay(new Overlay()
-                {
-                    @Override
-                    public void render(GuiGraphics guiGraphics, int i, int j, float f)
-                    {
-                        ErrorScreen errorScreen = new CustomErrorScreen(exception.getExceptions());
-                        errorScreen.init(client,
-                            client.getWindow().getGuiScaledWidth(),
-                            client.getWindow().getGuiScaledHeight());
-                        errorScreen.render(guiGraphics, i, j, f);
-                    }
-                }));
-        });
+        // Fabric events are registered via mixins.
     }
 }
